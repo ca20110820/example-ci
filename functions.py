3b862a26bb3414137f1d5d44620ef8ca4eea987f
ca20110820 <ca20110820@gmail.com>
2024-03-20 17:34:30 +0800

    feat: Add amazing_function

--- functions.py
+++ functions.py
@@ -9,3 +9,7 @@ def multiply(a, b):
 
 def convert_fahrenheit_to_celsius(fahrenheit):
     return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
+
+
+def amazing_function():
+    print("This is an amazing function!")
